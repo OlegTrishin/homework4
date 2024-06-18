806f50ec4b3e050d63bbc66f1cb34a040c16caec
Oleg <otrishin@gmail.com>
2024-06-18 21:14:37 +1000

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите любое слово: ")
+print(len(my_string))                           # Выводим длину символов введённого текста
+print(my_string.upper())                        # Выводим строку my_string в верхнем регистре
+print(my_string.lower())                        # Выводим строку my_string в нижнем регистре
+print(my_string.replace(" ", ""))   # Изменяем строку my_string, удалив все пробелы
+print(my_string[0])                             # Выводим первый символ строки my_string
+print(my_string[-1])                            # Выводим последний символ строки my_string
